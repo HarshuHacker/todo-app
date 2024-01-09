7f1470b8bf2b7f282a9fafba276b9cc05af99e66
Harsh Prasad <harshisindian.com>
2024-01-09 16:04:47 +0530

    Using Async Thunk Using Extra Reducers

--- src/redux/reducers/todoReducer.js
+++ src/redux/reducers/todoReducer.js
@@ -2,6 +2,22 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 // import { ADD_TODO, TOGGLE_TODO } from "../actions/todoActions";
 
+// export const getInitialStateAsync = createAsyncThunk(
+//   "todos",
+//   (arg, thunkApi) => {
+//     axios
+//       .get(arg)
+//       .then((response) =>
+//         thunkApi.dispatch(todoActions.initializeTodos(response.data))
+//       );
+//   }
+// );
+
+export const getInitialStateAsync = createAsyncThunk("todos", async (arg) => {
+  const { data } = await axios.get(arg);
+  return data;
+});
+
 const initialState = {
   todos: [
     { text: "Go to Gym at 6", completed: false },
@@ -32,16 +48,16 @@ const todoSlice = createSlice({
       });
     },
   },
+  extraReducers: (builder) => {
+    builder.addCase(getInitialStateAsync.fulfilled, (state, action) => {
+      state.todos = [...action.payload];
+    });
+  },
 });
 
 export const todoReducer = todoSlice.reducer;
 export const todoActions = todoSlice.actions;
 export const todoSelector = (state) => state.todos.todos;
-
-export const getInitialStateAsync = createAsyncThunk(todoActions.initializeTodos, (arg, thunkApi) => {
-  axios.get(arg)
-  .then(response => thunkApi.dispatch(todoActions.initializeTodos(response.data)))
-})
 
 // Creating Reducer Using Redux
 
